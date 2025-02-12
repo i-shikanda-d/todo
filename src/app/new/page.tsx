1ecff73685bc70a357bca4920c4d16fbfa380749
i-shikanda-d <i.shikanda.d@gmail.com>
2025-02-12 18:37:21 +0300

    implement ToDo creation and listing with toggle functionality

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,46 @@
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+async function createToDo(data: FormData) {
+  "use server"
+
+  const title = data.get("title")?.valueOf();
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid title");
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+
+  redirect("/")
+}
+
 export default function New() {
   return (
     <>
-      <p1>New</p1>
+      <header>New</header>
+      <form action={createToDo}>
+        <input
+          type="text"
+          name="title"
+          id=""
+          className="border bg-transparent border-slate-200"
+        />
+      </form>
+      <div>
+        <Link
+          href="/"
+          className="text-2xl font-semibold bg-red-500 px-9 rounded-md shadow-lg hover:bg-red-300 focus-within:bg-red-300"
+        >
+          Cancel
+        </Link>
+        <button
+          type='submit'
+          className="text-2xl font-semibold bg-sky-400 px-9 rounded-md shadow-lg hover:bg-orange-500 focus-within:bg-orange-500"
+        >
+          Create
+        </button>
+      </div>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+
 import { prisma } from "@/db";
 import Link from "next/link";
+import { TodoItem } from "./components/TodoItem";
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { complete} })
+  
+}
 
 export default async function Home() {
-  const todos = await prisma.todo.findmany();
+  const todos = await getTodos();
 
   return (
     <>
@@ -12,10 +25,8 @@ export default async function Home() {
       </header>
       <ul>
         {todos.map(todo => (
-          <li key={todo.id}>
-            {todo.title}
-          </li>
-        ) )}
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          ))}
       </ul>
     </>
   );
